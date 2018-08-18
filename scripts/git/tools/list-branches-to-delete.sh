6fade48fb9485ab692d7318a6472731f07f1eb67
Olegzandr VD <alex@oresoftware.com>
2018-08-17 20:56:08 -0700

    pdev:'set'

--- scripts/git/tools/list-branches-to-delete.sh
+++ scripts/git/tools/list-branches-to-delete.sh
@@ -3,12 +3,18 @@
 
 git fetch origin;
 
-#git branch --contains "$(git rev-parse "remotes/origin/dev")"
+echo; echo "Current branch: $(git rev-parse --abbrev-ref HEAD)"
 
+echo; echo "Branches that have been merged with origin/dev:";
 
-#git branch | tr -d ' *' | while read branch; do
-#     echo "$branch";
-#done
+git branch --merged "remotes/origin/dev" | tr -d ' *' | while read branch; do
+     echo "$branch";
+done
 
+echo; echo "Branches that have been merged with HEAD:";
 
-git branch --merged "remotes/origin/dev"
+git branch --merged HEAD | tr -d ' *' | while read branch; do
+     echo "$branch";
+done
+
+echo;
